2a75844712aff69bb7bcd601d4239cbbc098fc03
cliff.neames <cliff.neames@gmail.com>
2017-05-26 10:26:41 -0500

    Add project files.

---
+++ MacroTrackr/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MacroTrackr
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
